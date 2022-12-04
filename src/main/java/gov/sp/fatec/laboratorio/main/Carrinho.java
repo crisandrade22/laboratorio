b52f79148bd24a79865d3c729429ffa537d7f893
crisandrade22 <crisdsandrade@gmail.com>
2022-12-04 09:43:27 -0300

    Alteração de listview para tableview, aplicação ainda está quebrada

--- src/main/java/gov/sp/fatec/laboratorio/main/Carrinho.java
+++ src/main/java/gov/sp/fatec/laboratorio/main/Carrinho.java
@@ -2,10 +2,15 @@ package gov.sp.fatec.laboratorio.main;
 
 import gov.sp.fatec.laboratorio.model.Produto;
 import gov.sp.fatec.laboratorio.model.Sacola;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.*;
@@ -14,7 +19,21 @@ import java.util.*;
 public class Carrinho implements Initializable {
 
     @FXML
-    private ListView<String> listCarrinho;
+    private TableView<Produto> listCarrinho;
+
+    @FXML
+    private TableColumn<Produto, String> columnProduto;
+
+    @FXML
+    private TableColumn<Produto, Integer> columnQuantidade;
+
+    @FXML
+    private TableColumn<Produto, String> columnValorTotal;
+
+    @FXML
+    private TableColumn<Produto, String> columnUnitario;
+
+    ObservableList<Produto> insurances = FXCollections.observableArrayList();
 
     @FXML
     Label myLabel;
@@ -45,18 +64,26 @@ public class Carrinho implements Initializable {
         }
 
         String totalPagar;
-        listCarrinho.getItems().addAll("Produto                 Preço Unitário              Preço Total");
+//        listCarrinho.getItems().addAll("Produto                 Preço Unitário              Preço Total");
+        insurances.addAll(, "Cachaça");
+        listCarrinho.getColumns().addAll(columnProduto, columnQuantidade, columnUnitario, columnValorTotal);
+
         for (Map.Entry<Produto, Integer> entry : Sacola.getInstance().entradas()) {
-            System.out.println("Get preço produto: " + entry.getKey().getPrecoProduto() + "Get entry value: " + entry.getValue());
-            totalPagar = String.format("%.2f", entry.getKey().getPrecoProduto() * entry.getValue());
+            columnProduto.setCellValueFactory(new PropertyValueFactory(entry.getKey().getNomeProduto()));
+//            columnQuantidade.setCellValueFactory(new PropertyValueFactory(entry.getKey().);
+//            columnUnitario.setCellValueFactory(new PropertyValueFactory(entry.getKey().getNomeProduto()));
+//            columnValorTotal.setCellValueFactory(new PropertyValueFactory(entry.getKey().getNomeProduto()));
+            listCarrinho.getColumns().addAll(columnProduto);
+//            System.out.println("Get preço produto: " + entry.getKey().getPrecoProduto() + "Get entry value: " + entry.getValue());
+//            totalPagar = String.format("%.2f", entry.getKey().getPrecoProduto() * entry.getValue());
 
-            listCarrinho.getItems().addAll(entry.getKey().getNomeProduto() + "                     " + entry.getKey().getPrecoProduto() + "                             " + totalPagar);
+//            listCarrinho.getItems().addAll(entry.getKey().getNomeProduto() + "                     " + entry.getKey().getPrecoProduto() + "                             " + totalPagar);
         }
     }
 
-    public void adicionarCarrinho(javafx.scene.input.MouseEvent mouseEvent) {
-        for (String item : listCarrinho.getItems()) {
-            System.out.println("Item: " + item);
-        }
-    }
+//    public void adicionarCarrinho(javafx.scene.input.MouseEvent mouseEvent) {
+//        for (String item : listCarrinho.getItems()) {
+//            System.out.println("Item: " + item);
+//        }
+//    }
 }
